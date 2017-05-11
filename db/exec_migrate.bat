8f42fc6750014d963bb4389423711ba6c059aaca
Pablo Matas Gomez <pablomatiasgomez@gmail.com>
2017-05-10 23:12:50 -0300

    fix name

--- db/exec_migrate.bat
+++ db/exec_migrate.bat
@@ -1,1 +1,1 @@
-sqlcmd -S localhost\SQLSERVER2012 -U gd -P gd2017 -i script_creacion_inicial.sql -a 32767 -o result_migrate.txt
+sqlcmd -S localhost\SQLSERVER2012 -U gd -P gd2017 -i migrate.sql -a 32767 -o result_migrate.txt
