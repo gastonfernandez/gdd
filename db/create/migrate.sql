0cc172fae4c457e60bc00b7462f115296091d0fb
Pablo Matas Gomez <pablomatiasgomez@gmail.com>
2017-05-20 10:21:49 -0300

    arranco la migracion

--- db/create/migrate.sql
+++ db/create/migrate.sql
@@ -30,8 +30,8 @@ GO
 CREATE TABLE OSNR.Turno (
 	tur_id int IDENTITY(1,1) PRIMARY KEY,
 	tur_descripcion nvarchar(255) NOT NULL,
-	tur_hora_inicio time NOT NULL,
-	tur_hora_fin time NOT NULL,
+	tur_hora_inicio numeric(18,0) NOT NULL,
+	tur_hora_fin numeric(18,0) NOT NULL,
 	tur_valor_km numeric(18,2) NOT NULL,
 	tur_precio_base numeric(18,2) NOT NULL,
 	tur_habilitado bit DEFAULT 1 NOT NULL
@@ -40,7 +40,7 @@ GO
 
 CREATE TABLE OSNR.Usuario (
 	usu_id int IDENTITY(1,1) PRIMARY KEY,
-	usu_dni numeric(18, 0) NOT NULL,
+	usu_dni numeric(18, 0) UNIQUE NOT NULL,
 	usu_nombre nvarchar(255) NOT NULL,
 	usu_apellido nvarchar(255) NOT NULL,
 	usu_direccion nvarchar(255) NOT NULL,
@@ -58,24 +58,24 @@ GO
 
 CREATE TABLE OSNR.Chofer (
 	cho_id int IDENTITY(1,1) PRIMARY KEY,
-	cho_id_usuario int REFERENCES OSNR.Usuario NOT NULL
+	cho_id_usuario int REFERENCES OSNR.Usuario UNIQUE NOT NULL
 	)
 GO
 
 CREATE TABLE OSNR.Cliente (
 	cli_id int IDENTITY(1,1) PRIMARY KEY,
-	cli_id_usuario int REFERENCES OSNR.Usuario NOT NULL
+	cli_id_usuario int REFERENCES OSNR.Usuario UNIQUE NOT NULL
 	)
 GO
 
 CREATE TABLE OSNR.Auto (
 	aut_id int IDENTITY(1,1) PRIMARY KEY,
 	aut_id_modelo int REFERENCES OSNR.Modelo NOT NULL,
+	aut_id_chofer int REFERENCES OSNR.Chofer NOT NULL,
 	aut_patente nvarchar(255) NOT NULL,
 	aut_licencia nvarchar(255) NOT NULL,
 	aut_rodado nvarchar(255) NOT NULL,
-	aut_habilitado bit DEFAULT 1 NOT NULL,
-	aut_id_chofer int REFERENCES OSNR.Chofer NOT NULL
+	aut_habilitado bit DEFAULT 1 NOT NULL
 	)
 GO
 
@@ -114,8 +114,8 @@ CREATE TABLE OSNR.RendicionViaje (
 
 CREATE TABLE OSNR.Factura (
 	fac_id int IDENTITY(1,1) PRIMARY KEY,
-	fac_numero int NOT NULL,
-	fac_fecha datetime NOT NULL, /* TODO: CUANTAS FECHAS HAY?? */
+	fac_numero int UNIQUE NOT NULL,
+	fac_fecha datetime NOT NULL,
 	fac_fecha_inicio datetime NOT NULL,
 	fac_fecha_fin datetime NOT NULL,
 	fac_importe numeric(18,2) NOT NULL,
@@ -207,3 +207,119 @@ INSERT INTO OSNR.RolPorUsuario(id_rol, nombre_usuario)
 	values(1, 2)
 GO
 */
+
+
+/*****************************************************************/
+/************************** MIGRACION ****************************/
+/*****************************************************************/
+
+
+/* Marca */
+INSERT INTO OSNR.Marca
+	SELECT DISTINCT Auto_Marca
+	FROM gd_esquema.Maestra
+GO
+
+/* Modelo */
+INSERT INTO OSNR.Modelo
+	SELECT DISTINCT Auto_Modelo, mar_id
+	FROM gd_esquema.Maestra, OSNR.Marca
+	WHERE mar_nombre = Auto_Marca
+GO
+
+
+/* Usuarios */
+/* Tomamos como usuario el nombre+apellido y como password su nombre */
+INSERT INTO OSNR.Usuario
+	SELECT DISTINCT
+		Chofer_Dni
+		Chofer_Nombre,
+		Chofer_Apellido,
+		Chofer_Direccion,
+		Chofer_Telefono,
+		Chofer_Mail,
+		Chofer_Fecha_Nac,
+		Chofer_Nombre + Chofer_Apellido,
+		HashBytes('SHA2_256',convert(varchar(255), Chofer_Nombre)),
+		0, /* Intentos login */
+		1  /* Habilitado */
+	FROM gd_esquema.Maestra
+GO
+
+INSERT INTO OSNR.Usuario
+	SELECT DISTINCT
+		Cliente_Dni,
+		Cliente_Nombre,
+		Cliente_Apellido,
+		Cliente_Direccion,
+		Cliente_Telefono,
+		Cliente_Mail,
+		Cliente_Fecha_Nac,
+		Cliente_Nombre + Cliente_Apellido,
+		HashBytes('SHA2_256',convert(varchar(255), Cliente_Nombre)),
+		0, /* Intentos login */
+		1  /* Habilitado */
+	FROM gd_esquema.Maestra
+GO
+
+
+/* Chofer */
+INSERT INTO OSNR.Chofer
+	SELECT DISTINCT usu_id
+	FROM gd_esquema.Maestra, OSNR.Usuario
+	WHERE usu_dni = Chofer_Dni
+GO
+
+/* Cliente */
+INSERT INTO OSNR.Cliente
+	SELECT DISTINCT usu_id
+	FROM gd_esquema.Maestra, OSNR.Usuario
+	WHERE usu_dni = Cliente_Dni
+GO
+
+/* Auto */
+INSERT INTO OSNR.Auto
+	SELECT DISTINCT mod_id, cho_id, Auto_Patente, Auto_Licencia, Auto_Rodado, 1
+	FROM gd_esquema.Maestra, OSNR.Modelo, OSNR.Usuario, OSNR.Chofer
+	WHERE mod_nombre = Auto_Modelo AND usu_dni = Chofer_Dni AND cho_id_usuario = usu_id
+GO
+
+/* Turno */
+INSERT INTO OSNR.Turno
+	SELECT DISTINCT Turno_Descripcion, Turno_Hora_Inicio, Turno_Hora_Fin, Turno_Valor_Kilometro, Turno_Precio_Base, 1
+	FROM gd_esquema.Maestra
+GO
+
+/* AutoTurno */
+INSERT INTO OSNR.AutoTurno
+	SELECT DISTINCT aut_id, tur_id
+	FROM gd_esquema.Maestra, OSNR.Auto, OSNR.Turno
+	WHERE aut_patente = Auto_Patente AND tur_descripcion = Turno_Descripcion
+GO
+
+/* Vije */
+INSERT INTO OSNR.Viaje
+	SELECT DISTINCT Viaje_Cant_Kilometros, Viaje_Fecha, 1, cli_id, aut_id
+	FROM gd_esquema.Maestra, OSNR.Cliente, OSNR.Usuario, OSNR.Auto
+	WHERE Viaje_Cant_Kilometros IS NOT NULL AND aut_patente = Auto_Patente 
+	/* TODO CHOFER .. */
+	AND usu_dni = Cliente_Dni AND cli_id_usuario = usu_id
+GO
+
+/* Factura */
+INSERT INTO OSNR.Factura
+	/* TODO: Importe total, podriamos sacarlo incluso... */
+	SELECT DISTINCT Factura_Nro, Factura_Fecha, Factura_Fecha_Inicio, Factura_Fecha_Fin, 100, 1
+	FROM gd_esquema.Maestra, OSNR.Usuario, OSNR.Cliente
+	WHERE Factura_Nro IS NOT NULL AND usu_dni = Cliente_Dni AND cli_id_usuario = usu_id
+GO
+
+/* FacturaViaje */
+INSERT INTO OSNR.FacturaViaje
+	SELECT DISTINCT fac_id, via_id /* TODO ESTO NO PARECERIA ANDAR */
+	FROM gd_esquema.Maestra, OSNR.Factura, OSNR.Viaje
+	WHERE Factura_Nro IS NOT NULL AND Factura_Nro = fac_numero AND Viaje_Fecha = via_fecha
+GO
+
+
+ 
