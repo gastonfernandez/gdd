c1b0b87afa83cb7466890c67a096d197ed305f68
Agustin Szyszczyc <aszyszczyc@ad.kimn.com.ar>
2017-05-24 23:42:39 -0300

    se sube el login y la config de la base de datos. no esta testeado, falta testear un login correcto y poner valores reales a la conexion con la base

---
+++ UberFrba/Mappings/Usuario.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using UberFrba.Mappings;
+
+namespace UberFrba.Mappings
+{
+
+    class Usuario
+    {
+        public Int32 Id = 0;
+        public Int32 dni;
+        public String nombre;
+        public String apellido;
+        public String direccion;
+        public String telefono;
+        public String mail;
+        public DateTime fecNac;
+        public String username = "";
+        public String pass = "";
+        public Int32 intentos;
+        public Int32 habilitado;
+
+
+        public Usuario getUser(String login, String pass)
+        {
+            string valorEncriptado = SHA256Encripta(pass);
+
+            #region ValidarUsuarioyPass Contra la base
+            BaseDeDatos db = new BaseDeDatos();
+
+            DataTable dt = db.select_query("  select usu_id,usu_dni,usu_nombre,usu_apellido,usu_direccion ,usu_telefono,usu_mail,usu_fecha_nacimiento,usu_login,usu_password,usu_cantidad_intentos,usu_habilitado from [#GDD].USUARIO where username= '" + login + "'");
+
+            if (dt.Rows.Count > 1)
+            {
+                throw new Exception("Se produjo un problema al intentar iniciar sesion por favor concatese con el administrador");
+
+            }
+            else
+            {
+                if (dt.Rows.Count == 0)
+                {
+                    throw new Exception("El usuario ingresado es inexistente");
+                }
+                else
+                {
+
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        this.Id = Convert.ToInt32(row["usu_id"]);
+                        this.dni = Convert.ToInt32(row["usu_dni"]);
+                        this.nombre = Convert.ToString(row["usu_nombre"]);
+                        this.apellido = Convert.ToString(row["usu_apellido"]);
+                        this.direccion = Convert.ToString(row["usu_direccion"]);
+                        this.telefono = Convert.ToString(row["usu_telefono"]);
+                        this.mail = Convert.ToString(row["usu_mail"]);
+                        this.fecNac = Convert.ToDateTime(row["usu_fecha_nacimiento"]);
+                        this.username = Convert.ToString(row["usu_login"]);
+                        this.pass = Convert.ToString(row["usu_password"]);
+                        this.intentos = Convert.ToInt32(row["usu_cantidad_intentos"]);
+                        this.habilitado = Convert.ToInt32(row["usu_habilitado"]);
+
+                    }
+                }
+            }
+
+            if (this.habilitado == 0)
+            {
+                throw new Exception("El usuario se encuentra bloqueado");
+            }
+            else
+            {
+                #region CompararValor Ingresado contra la base
+                String Msg = String.Empty;
+                if (this.pass == valorEncriptado)
+                {
+                    Msg = "OK";
+                    this.habilitado = 1;
+                    this.intentos = 0;
+                }
+                else
+                {
+                    Msg = "ERR";
+                    if (this.intentos == 2)
+                        this.habilitado = 0;
+                    else
+                        this.intentos++;
+                }
+                #endregion
+
+                #region ModificarValor en base a lo procesado
+                string update = "update [#GDD].usuario " +
+                                 " set usu_cantidad_intentos= " + this.intentos + "," +
+                                 "usu_habilitado= " + this.habilitado +
+                                 " where usu_id= " + this.Id;
+                db.query(update);
+                #endregion
+
+                if (Msg == "ERR")
+                    return null;
+                else
+                    return this;
+            }
+            #endregion
+
+
+        }
+
+        private string SHA256Encripta(string input)
+        {
+            SHA256CryptoServiceProvider provider = new SHA256CryptoServiceProvider();
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashedBytes = provider.ComputeHash(inputBytes);
+
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < hashedBytes.Length; i++)
+                output.Append(hashedBytes[i].ToString("x2").ToLower());
+
+            return output.ToString();
+        }
+    }
+}
---
+++ UberFrba/Pagina Principal/InicioSesion.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UberFrba.Mappings;
+
+namespace UberFrba.Pagina_Principal
+{
+    public partial class InicioSesion : Form
+    {
+        public InicioSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void Ingresar_click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ingresar_Click(object sender, EventArgs e)
+        {
+            // Llamo al Menú(comentario anterior)
+            MenuPpal frm = new Menu.MenuPpal();//Comentario actual: no entiendo que deberia hacer esto.
+            this.Hide();
+            frm.Show();
+            /* el pass de prueba que use es AAAA para el usuario PEPE(comentario anterior)*/
+
+            try
+            {
+                #region ValidarParametros y Usuario
+                if (!validoParametros(textoUsuario, textoPassword))
+                {
+                    MessageBox.Show("Ingrese todos los valores");
+
+                }
+                else
+                {
+
+                    #region ObtenerUsuario
+                    Usuario usu = new Usuario();
+
+                    usu = usu.getUser(textoUsuario.Text, textoPassword.Text);
+                    #endregion
+
+                    if (usu == null)
+                        MessageBox.Show("La contraseña ingresada es incorrecta");
+                    else
+                        MessageBox.Show("Se ha logueado correctamente"); /* aca hay que hacer lo que siga*/
+
+
+                }
+
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); //throw new Exception("Se ha producido un error en el momento de realizar el logueo consulte al administrador" + ex.Message);
+            }
+
+        }
+
+        private Boolean validoParametros(TextBox usuario, TextBox pass)
+        {
+            if (usuario.Text == string.Empty || pass.Text == string.Empty)
+                return false;
+            else
+                return true;
+        }
+
+    }
+}
