e1eee30483bddba1d2252d9f47692edfbc390541
Pablo Matas Gomez <pablomatiasgomez@gmail.com>
2017-06-08 21:12:59 -0300

    moving packages

--- db/create/exec_migrate.bat
+++
@@ -1,1 +0,0 @@
-sqlcmd -S localhost\SQLSERVER2012 -U gd -P gd2017 -i migrate.sql -a 32767 -o result_migrate.txt
--- db/drop/exec_drop.bat
+++
@@ -1,1 +0,0 @@
-sqlcmd -S localhost\SQLSERVER2012 -U gd -P gd2017 -i drop.sql -a 32767 -o result_drop.txt
